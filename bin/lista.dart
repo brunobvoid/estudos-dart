1e786a033aca004fb1229c25ef4071fa59056115
Bruno Bastos <115432374+brunobvoid@users.noreply.github.com>
2023-03-15 11:51:30 -0300

    estudos em dart

--- bin/lista.dart
+++ bin/lista.dart
@@ -5,8 +5,8 @@ void main(){
   List<dynamic> bruno = ['Bruno Bastos Silva', 28, 96.5, 1.77, true, 'Sujebs'];
   String frase = ('meu nome é ${bruno[0]} \n'
   'tenho ${bruno[1]} anos \n'
-  'minha altura é ${bruno[3]} m \n'
-  'peso ${bruno[2]} kg \n'
+  'minha altura é ${bruno[3]}m \n'
+  'peso ${bruno[2]}kg \n'
   'me considero geek? ${bruno[4]} \n'
   'meu apelido é ${bruno[5]}');
 
