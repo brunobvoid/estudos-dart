97c491caca241f1c1edb38b4707dfcac30f0bbd7
Bruno Bastos <115432374+brunobvoid@users.noreply.github.com>
2023-03-15 10:41:50 -0300

    Iniciando estudos em dart

---
+++ bin/projetos.dart
@@ -0,0 +1,10 @@
+void main() {
+  String nome = 'Bruno';
+  int idade = 28;
+  double peso = 96.5;
+  double altura = 1.77;
+  print('meu nome é $nome');
+  print('tenho $idade anos');
+  print('minha altura é $altura m');
+  print('peso $peso kg');
+}
---
+++ test/projetos_test.dart
@@ -0,0 +1,8 @@
+import 'package:projetos/projetos.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
